72dbcbb22d41d2c0580d0a1b00cbbeba1598743d
刘昌喜 <liuchangxi@querycap.com>
2020-02-19 19:55:29 +0800

    commit

---
+++ leetcode/11container-with-most-water.go
@@ -0,0 +1,47 @@
+package leetcode
+
+/*
+给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+说明：你不能倾斜容器，且 n 的值至少为 2。
+
+
+
+图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+
+
+
+示例:
+
+输入: [1,8,6,2,5,4,8,3,7]
+输出: 49
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/container-with-most-water
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func GetArea(height []int, start int, end int) int {
+	min := 0
+	if height[start] < height[end] {
+		min = height[start]
+	} else {
+		min = height[end]
+	}
+
+	return (end - start) * min
+
+}
+
+func MaxArea(height []int) int {
+	maxData := 0
+	for i := 0; i < len(height); i++ {
+		for j := i + 1; j < len(height); j++ {
+			data := GetArea(height, i, j)
+			if data > maxData {
+				maxData = data
+			}
+		}
+	}
+	return maxData
+}
